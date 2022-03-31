cd69e65590cf01f78e974b2391fd05aeef2ccb6e
Thanh Nguyen <puffle.nnt@gmail.com>
2022-03-31 20:40:05 +0700

    set up user entity and resolver

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,72 @@
-import { MikroORM } from '@mikro-orm/core'
-import { __prod__ } from './constants';
-import mikroOrmConfig from './mikro-orm.config';
-import express from 'express';
-import { ApolloServer } from 'apollo-server-express';
-import { buildSchema } from 'type-graphql'
-import { HelloResolver } from './resolvers/hello';
-import { PostResolver } from './resolvers/post';
-
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import mikroOrmConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
+import { UserResolver } from "./resolvers/user";
+import { createClient } from "redis";
+import session from "express-session";
+import connectRedis from "connect-redis";
+import { MyContext } from "./types";
 
 const main = async () => {
-    const orm = await MikroORM.init(mikroOrmConfig);
-    orm.getMigrator().up();
+  const orm = await MikroORM.init(mikroOrmConfig);
+  orm.getMigrator().up();
 
-    const app = express();
+  const app = express();
 
-    const apolloServer = new ApolloServer({
-        schema: await buildSchema({
-            resolvers: [HelloResolver, PostResolver],
-            validate: false
-        }),
-        context: () => ({ em: orm.em })
-    });
+  app.set("trust proxy", !__prod__);
 
-    await apolloServer.start();
-    apolloServer.applyMiddleware({ app });
+  const RedisStore = connectRedis(session);
 
-    app.listen(4000, () => {
-        console.log('server started on port 4000')
+  // redis@v4
+  const redisClient = createClient({ legacyMode: true });
+  redisClient.connect().catch(console.error);
+
+  app.use(
+    session({
+      name: "qid",
+      store: new RedisStore({
+        client: redisClient,
+        disableTouch: true,
+      }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10,
+        secure: true,
+        sameSite: "none",
+        httpOnly: true,
+      },
+      saveUninitialized: false,
+      secret: "keyboard cat",
+      resave: false,
     })
-}
+  );
 
-main().catch(err => {
-    console.error(err);
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver, UserResolver],
+      validate: false,
+    }),
+    context: ({ req, res }): MyContext => ({ em: orm.em, req, res }),
+  });
+
+  await apolloServer.start();
+  apolloServer.applyMiddleware({
+    app,
+    cors: {
+      origin: "https://studio.apollographql.com",
+      credentials: true,
+    },
+  });
+
+  app.listen(4000, () => {
+    console.log("server started on port 4000");
+  });
+};
+
+main().catch((err) => {
+  console.error(err);
 });
