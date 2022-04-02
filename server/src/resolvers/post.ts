770299c50c3c9b24c83c7c83f6503a66d856b5af
Thanh Nguyen <puffle.nnt@gmail.com>
2022-04-02 22:37:43 +0700

    integrate server side rendering

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -4,58 +4,52 @@ import { Arg, Ctx, Mutation, Query, Resolver } from "type-graphql";
 
 @Resolver()
 export class PostResolver {
-    @Query(() => [Post])
-    posts(
-        @Ctx() { em }: MyContext
-    ): Promise<Post[]> {
-        return em.find(Post, {});
+  @Query(() => [Post])
+  async posts(@Ctx() { em }: MyContext): Promise<Post[]> {
+    return em.find(Post, {});
+  }
+
+  @Query(() => Post, { nullable: true })
+  post(@Arg("id") _id: number, @Ctx() { em }: MyContext): Promise<Post | null> {
+    return em.findOne(Post, { _id });
+  }
+
+  @Mutation(() => Post)
+  async createPost(
+    @Arg("title", () => String) title: string,
+    @Ctx() { em }: MyContext
+  ): Promise<Post | null> {
+    //@ts-ignore
+    const post = em.create(Post, { title });
+    await em.persistAndFlush(post);
+
+    return em.findOne(Post, { title });
+  }
+
+  @Mutation(() => Post, { nullable: true })
+  async updatePost(
+    @Arg("id") _id: number,
+    @Arg("title", () => String, { nullable: true }) title: string,
+    @Ctx() { em }: MyContext
+  ): Promise<Post | null> {
+    const post = await em.findOne(Post, { _id });
+    if (!post) {
+      return null;
     }
 
-    @Query(() => Post, { nullable: true })
-    post(
-        @Arg('id') _id: number,
-        @Ctx() { em }: MyContext
-    ): Promise<Post | null> {
-        return em.findOne(Post, { _id });
+    if (typeof title !== "undefined") {
+      post.title = title;
+      await em.persistAndFlush(post);
     }
+    return post;
+  }
 
-    @Mutation(() => Post)
-    async createPost(
-        @Arg('title', () => String) title: string,
-        @Ctx() { em }: MyContext
-    ): Promise<Post | null> {
-        //@ts-ignore
-        const post = em.create(Post, { title })
-        await em.persistAndFlush(post)
-
-        return em.findOne(Post, { title });
-    }
-
-    @Mutation(() => Post, { nullable: true })
-    async updatePost(
-        @Arg('id') _id: number,
-        @Arg('title', () => String, { nullable: true }) title: string,
-        @Ctx() { em }: MyContext
-    ): Promise<Post | null> {
-        const post = await em.findOne(Post, { _id });
-        if (!post) {
-            return null
-        }
-
-        if (typeof title !== 'undefined') {
-            post.title = title;
-            await em.persistAndFlush(post);
-        }
-        return post;
-
-    }
-
-    @Mutation(() => Boolean)
-    async deletePost(
-        @Arg('id') _id: number,
-        @Ctx() { em }: MyContext
-    ): Promise<boolean> {
-        await em.nativeDelete(Post, { _id });
-        return true;
-    }
+  @Mutation(() => Boolean)
+  async deletePost(
+    @Arg("id") _id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<boolean> {
+    await em.nativeDelete(Post, { _id });
+    return true;
+  }
 }
--- web/src/components/NavBar.tsx
+++ web/src/components/NavBar.tsx
@@ -2,10 +2,13 @@ import { Box, Button, Flex } from "@chakra-ui/react";
 import Link from "next/link";
 import React from "react";
 import { useLogoutMutation, useMeQuery } from "../generated/graphql";
+import { isServer } from "../utils/isServer";
 
 const NavBar = () => {
   const [{ fetching: logoutFetching }, logout] = useLogoutMutation();
-  const [{ data, fetching }] = useMeQuery();
+  const [{ data, fetching }] = useMeQuery({
+    pause: isServer(),
+  });
   let body = null;
 
   if (fetching) {
